1a363598eed5cc1152a9b82cfa05e86ee4ff3563
Andrii <shaposhnikandriy@gmail.com>
2018-04-24 02:44:56 +0300

    FrontController has been added

---
+++ src/main/java/com/epam/salon/controller/FrontControllerServlet.java
@@ -0,0 +1,45 @@
+package com.epam.salon.controller;
+
+import com.epam.salon.controller.command.FrontCommand;
+import com.epam.salon.controller.command.UnknownCommand;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/front")
+public class FrontControllerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        handler(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        handler(req, resp);
+    }
+
+    private FrontCommand getCommand(HttpServletRequest request) {
+        try {
+            Class type = Class.forName(String.format(
+                    "com.epam.salon.controller.command.%sCommand",
+                    request.getParameter("command")
+            ));
+
+            return (FrontCommand) type.asSubclass(FrontCommand.class).newInstance();
+
+        } catch (Exception e) {
+            return new UnknownCommand();
+        }
+    }
+
+    private void handler(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        FrontCommand command = getCommand(req);
+        command.init(getServletContext(), req, resp);
+        command.process();
+    }
+}
---
+++ src/main/java/com/epam/salon/controller/command/FrontCommand.java
@@ -0,0 +1,26 @@
+package com.epam.salon.controller.command;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public abstract class FrontCommand {
+    protected ServletContext context;
+    protected HttpServletRequest request;
+    protected HttpServletResponse response;
+
+    public void init(ServletContext context, HttpServletRequest request, HttpServletResponse response ) {
+        this.context = context;
+        this.request = request;
+        this.response = response;
+    }
+
+    public abstract void process() throws ServletException, IOException;
+
+    protected void forward(String target) throws ServletException, IOException {
+        target = String.format("/WEB-INF/pages/%s.jsp", target);
+        context.getRequestDispatcher(target).forward(request, response);
+    }
+}
