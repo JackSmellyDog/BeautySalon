0668b77f34cf314adffaf4bc8b23be8b23297554
Andrii <shaposhnikandriy@gmail.com>
2018-05-27 20:38:49 +0300

    + review queries

--- src/main/java/com/kpi/salon/dao/impl/ReviewDao.java
+++ src/main/java/com/kpi/salon/dao/impl/ReviewDao.java
@@ -17,12 +17,6 @@ import static com.kpi.salon.dao.DaoFactory.Entity.REQUEST;
 public class ReviewDao implements IReviewsDao {
     private static final Logger LOGGER = Logger.getLogger(ReviewDao.class);
 
-    private static final String INSERT_REVIEW_QUERY = "INSERT INTO beauty_reviews (text, rating, request_id) VALUES (?, ?, ?)";
-    private static final String ALL_REVIEWS_QUERY = "SELECT * FROM beauty_reviews";
-    private static final String FIND_BY_ID_QUERY = "SELECT * FROM beauty_reviews WHERE id=?";
-    private static final String UPDATE_REVIEW_QUERY = "UPDATE beauty_reviews SET text=?, rating=? WHERE id=?";
-    private static final String FIND_BY_REQUEST_ID_QUERY = "SELECT * FROM beauty_reviews WHERE request_id=?";
-
     private ConnectionManager connectionManager = ConnectionManager.getInstance();
     private RequestDao requestDao = (RequestDao) DaoFactory.create(REQUEST);
     private Properties queries;
@@ -31,11 +25,11 @@ public class ReviewDao implements IReviewsDao {
         init();
     }
 
-
     @Override
     public List<Review> findAll() {
         List<Request> requests = requestDao.findAll();
         Map<Long, Request> map = new HashMap<>();
+        String query = queries.getProperty("sql.review.find.all");
 
         for (Request request : requests) {
             map.put(request.getId(), request);
@@ -46,7 +40,7 @@ public class ReviewDao implements IReviewsDao {
         try(Connection connection = connectionManager.getConnection();
             Statement statement = connection.createStatement()
         ){
-            ResultSet resultSet = statement.executeQuery(ALL_REVIEWS_QUERY);
+            ResultSet resultSet = statement.executeQuery(query);
             while (resultSet.next()){
                 reviews.add(new Review(
                         resultSet.getLong("id"),
@@ -56,7 +50,7 @@ public class ReviewDao implements IReviewsDao {
                 ));
             }
 
-        } catch (SQLException e) {
+        } catch (NullPointerException | SQLException e) {
             LOGGER.error(e.getMessage(), e);
         }
 
@@ -71,9 +65,10 @@ public class ReviewDao implements IReviewsDao {
     @Override
     public Review findById(Long id) {
         Review review = null;
+        String query = queries.getProperty("sql.review.find.by.id");
 
         try(Connection connection = connectionManager.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_ID_QUERY)
+            PreparedStatement preparedStatement = connection.prepareStatement(query)
         ){
             preparedStatement.setLong(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -90,7 +85,7 @@ public class ReviewDao implements IReviewsDao {
                 }
             }
 
-        } catch (SQLException e) {
+        } catch (NullPointerException | SQLException e) {
             LOGGER.error(e.getMessage(), e);
         }
 
@@ -99,8 +94,10 @@ public class ReviewDao implements IReviewsDao {
 
     @Override
     public boolean insert(Review item) {
+        String query = queries.getProperty("sql.review.insert");
+
         try(Connection connection = connectionManager.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_REVIEW_QUERY)
+            PreparedStatement preparedStatement = connection.prepareStatement(query)
         ){
             preparedStatement.setString(1, item.getText());
             preparedStatement.setInt(2, item.getRating());
@@ -109,7 +106,7 @@ public class ReviewDao implements IReviewsDao {
 
             return true;
 
-        } catch (SQLException e) {
+        } catch (NullPointerException | SQLException e) {
             LOGGER.error(e.getMessage(), e);
             return false;
         }
@@ -117,8 +114,9 @@ public class ReviewDao implements IReviewsDao {
 
     @Override
     public boolean update(Review item) {
+        String query = queries.getProperty("sql.review.update");
         try(Connection connection = connectionManager.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_REVIEW_QUERY)
+            PreparedStatement preparedStatement = connection.prepareStatement(query)
         ){
             preparedStatement.setString(1, item.getText());
             preparedStatement.setInt(2, item.getRating());
@@ -128,7 +126,7 @@ public class ReviewDao implements IReviewsDao {
 
             return true;
 
-        } catch (SQLException e) {
+        } catch (NullPointerException | SQLException e) {
             LOGGER.error(e.getMessage(), e);
             return false;
         }
@@ -137,9 +135,10 @@ public class ReviewDao implements IReviewsDao {
     @Override
     public Review findByRequestId(Request request) {
         Review review = null;
+        String query = queries.getProperty("sql.review.find.by.request");
 
         try(Connection connection = connectionManager.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_REQUEST_ID_QUERY)
+            PreparedStatement preparedStatement = connection.prepareStatement(query)
         ){
             preparedStatement.setLong(1, request.getId());
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -152,7 +151,7 @@ public class ReviewDao implements IReviewsDao {
                 review = new Review(id, text, rating, request);
             }
 
-        } catch (SQLException e) {
+        } catch (NullPointerException | SQLException e) {
             LOGGER.error(e.getMessage(), e);
         }
 
